901bdbf5f2a15bd359ca9bce65451317d551bc96
Stratis Dermanoutsos <stratis450@gmail.com>
2023-08-03 00:09:28 +0300

    Basic changed for PR
    Co-Authored-By: DIMITRIOS CHRYSOCHERIS <p19190@cslab.unipi.gr>

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+    app.on('pull_request.closed', async context => {
+        const pullRequest = context.payload.pull_request;
+        if (pullRequest.merged) {
+            const filesChanged = await context.octokit.pulls.listFiles(context.repo({
+                pull_number: pullRequest.number
+            }));
+    
+            for (let file of filesChanged.data) {
+                const content = await context.octokit.repos.getContent(context.repo({
+                    path: file.filename,
+                    ref: pullRequest.head.sha
+                }));
+    
+                const fileContent = Buffer.from(content.data.toString(), 'base64').toString();
+                context.log(fileContent);
+            }
+        }
     });
-    await context.octokit.issues.createComment(issueComment);
-  });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+    // For more information on building apps:
+    // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+    // To get your app running against GitHub, see:
+    // https://probot.github.io/docs/development/
 };
